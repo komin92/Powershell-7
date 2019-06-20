575810032cadeb93992831c7fc43a3990dc06eb5
Braeden Hopper <BraedenHopper99@users.noreply.github.com>
2019-06-20 15:53:42 -0400

    Add files via upload

---
+++ Uninstall-Avast.ps1
@@ -0,0 +1,8 @@
+﻿$url = "https://files.avast.com/iavs9x/avastclear.exe"
+$output = "$PSScriptRoot/avastclear.exe"
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+$process = start-process $output -windowstyle Hidden -PassThru -Wait
+$output = $process.ExitCode
+Write-Output $output
