458cd900626bbcf6520d07fdaf832522142c1320
Braeden Hopper <BraedenHopper99@users.noreply.github.com>
2019-07-02 12:39:18 -0400

    Delete Uninstall-Avast.ps1

--- Uninstall-Avast.ps1
+++
@@ -1,8 +0,0 @@
-﻿$url = "https://files.avast.com/iavs9x/avastclear.exe"
-$output = "$PSScriptRoot/avastclear.exe"
-[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-$process = start-process $output -windowstyle Hidden -PassThru -Wait
-$Poutput = $process.ExitCode
-Write-Output $Poutput
