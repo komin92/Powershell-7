a4020c694d1f311114d8fc104ac7b6cc3f2c8100
Braeden Hopper <BraedenHopper99@users.noreply.github.com>
2019-07-23 15:54:42 -0400

    Delete Download-AvastClear.ps1

--- Download-AvastClear.ps1
+++
@@ -1,5 +0,0 @@
-﻿$url = "https://files.avast.com/iavs9x/avastclear.exe"
-$output = "$PSScriptRoot/avastclear.exe"
-[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
