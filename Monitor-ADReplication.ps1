dd762178cc9d23f558f0068a78ea384a8b8e7984
Braeden Hopper <BraedenHopper99@users.noreply.github.com>
2019-11-11 14:12:47 -0500

    Add files via upload

---
+++ Monitor-ADReplication.ps1
@@ -0,0 +1,10 @@
+﻿$AlertTime = (get-date).AddHours(-6)
+$FailedArr = @()
+$RepStatus = Get-ADReplicationPartnerMetadata -Target * -Partition * | Select-Object Server, Partition, Partner, ConsecutiveReplicationFailures, LastReplicationSuccess, LastRepicationResult
+foreach ($Report in $RepStatus) {
+    $Partner = $Report.partner -split "CN="
+    if ($report.LastReplicationSuccess -lt $AlertTime) {
+        $FailedArr += "$($Report.Server) could not replicate with partner $($Partner[2]) for 6 hours. please investigate"
+    }
+}
+if (!$FailedArr) { $FailedArr = "Healthy" }
