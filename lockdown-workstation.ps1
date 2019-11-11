c88565017cfd5252b0ed00024332b599748ed9c9
Braeden Hopper <BraedenHopper99@users.noreply.github.com>
2019-11-11 16:06:21 -0500

    Add files via upload

---
+++ lockdown-workstation.ps1
@@ -0,0 +1,11 @@
+﻿<#
+    .SYNOPSIS 
+        Firewall Configuration:
+            No incoming connections
+            Outgoing only
+#>
+
+## Start with Firewall 
+Set-NetFirewallProfile -All -Enabled true -ErrorAction Stop
+Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Allow -NotifyOnListen True -AllowUnicastResponseToMulticast True -LogFileName %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log
+
